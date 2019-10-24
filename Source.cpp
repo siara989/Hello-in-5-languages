1b90d20789188e2acbb2ad0fa60529fd421b83e9
chala2019 <52007759+chala2019@users.noreply.github.com>
2019-10-24 14:56:58 +0530

    Added french

--- Source.cpp
+++ Source.cpp
@@ -1,13 +1,14 @@
 #include<iostream>
 int main() {
 	using std::cout;
-	cout << "Display Hello in six languages\n";
+	cout << "Display Hello in seven languages\n";
 	cout << "1. English - Hello\n";
 	cout << "2. Sinhala - Ayubowan\n";
 	cout << "3. Malay - Salamat\n";
 	cout << "4. French - Bonjour\n";
 	cout << "5. Hindi - Namaste\n";
 	cout << "6. Spanish - Hola\n";
+	cout << "7. French - Salut\n";
 	system("pause");
 	return 0;
 }
